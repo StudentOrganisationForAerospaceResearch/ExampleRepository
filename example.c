095ceafbae565eba7c8614fe7d2d9147a65a42a9
Matt Allwright <matt.a.4890@gmail.com>
2018-10-27 10:42:53 -0600

    Added my name. Also formatted the file to use the superior formatting structure.

--- example.c
+++ example.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
-int main()
-{
-   // Add your name to be printed below
-   printf("Who has completed the tutorial?\n");
-   printf("Natalia\n");
+int main() {
 
-   return 0;
+	// Add your name to be printed below
+	printf("Who has completed the tutorial?\n");
+	printf("Natalia\n");
+	printf("Matt\n"); // Always null-terminate your strings!
+	
+	return 0;
+
 }
