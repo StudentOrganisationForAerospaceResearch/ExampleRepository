412b8050bc29c04c1ee81fc10436fc200e9b56c8
Matt Allwright <matt.a.4890@gmail.com>
2018-10-27 10:50:21 -0600

    Removed useless comment

--- example.c
+++ example.c
@@ -5,7 +5,7 @@ int main() {
 	// Add your name to be printed below
 	printf("Who has completed the tutorial?\n");
 	printf("Natalia\n");
-	printf("Matt\n"); // Always null-terminate your strings!
+	printf("Matt\n"); 
 	
 	return 0;
 
