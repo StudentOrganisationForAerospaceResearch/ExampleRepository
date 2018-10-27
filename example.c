dc5afa5e3e523709900d297269ffe45f4c87cfdd
missmarlos <marela.carlos@ucalgary.ca>
2018-10-27 11:11:14 -0600

    I made a commit.

--- example.c
+++ example.c
@@ -3,8 +3,8 @@
 int main()
 {
    // Add your name to be printed below
-   printf("Who has completed the tutorial?\n");
-   printf("Natalia\n");
-
+    printf("Who has completed the tutorial?\n");
+    printf("Natalia\n");
+    printf("Marela\n");
    return 0;
 }
